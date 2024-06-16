0380d3ab3f2e95ef3648218fd75cc17392123c05
OlaiGV <115138413+OlaiGV@users.noreply.github.com>
2024-06-16 11:31:36 +0200

    Subida inicial del proyecto

---
+++ javascript/operaciones-interes.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+  let paneles = document.querySelectorAll(".panel");
+  
+  paneles.forEach((panel) => {
+    panel.addEventListener("click", () => {
+      removeActiveClasses();
+      panel.classList.add("active");
+    });
+  });
+
+  function removeActiveClasses() {
+    paneles.forEach((panel) => {
+      panel.classList.remove("active");
+    });
+  }
+};
